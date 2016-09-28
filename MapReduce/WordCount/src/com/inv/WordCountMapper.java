033caec1cb3e0580a920e2b0801c5eefbac93a40
email2dorababu <email2dorababu@gmail.com>
2016-09-28 09:50:19 +0530

    msg

---
+++ MapReduce/WordCount/src/com/inv/WordCountMapper.java
@@ -0,0 +1,34 @@
+package com.inv;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
+
+	@Override
+	protected void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+		
+		String str=value.toString();
+
+		StringTokenizer tokens=new StringTokenizer(str," ");
+		
+		while(tokens.hasMoreTokens()){
+			
+			String str1=tokens.nextToken();
+			context.write(new Text(str1), new IntWritable(1));
+			
+		}
+		
+		
+		
+	}
+	
+	
+
+}
